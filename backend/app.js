3a3d261b7bedeb17f2609c4b1465dd0c50e2cae1
unknown <s0527670044@gmail.com>
2025-05-26 13:36:45 +0300

    create chat

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const userRoutes = require('./routes/userRoutes'); 
 const authRoutes = require('./routes/authRoutes'); 
@@ -11,6 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+const server = http.createServer(app);
+
 app.use(cors());
 app.use(express.json());
 
@@ -18,10 +22,30 @@ mongoose.connect(process.env.CONECTION_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('receiveMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/competitions', competitionRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ fronted/src/features/chat/chat.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+const Chat = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [input, setInput] = useState<string>('');
+  const socket: Socket = io('http://localhost:3500');
+
+  useEffect(() => {
+    socket.on('receiveMessage', (message: string) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off('receiveMessage');
+    };
+  }, [socket]);
+
+  const sendMessage = () => {
+    if (input.trim()) {
+      setMessages((prevMessages) => [...prevMessages, input]);
+      socket.emit('sendMessage', input);
+      setInput('');
+    }
+  };
+
+  return (
+    <div>
+        <h1>ouer chat</h1>
+      <div style={{ maxHeight: '400px', overflowY: 'scroll' }}>
+        {messages.map((msg, index) => (
+          <div key={index} style={{ padding: '5px', borderBottom: '1px solid #ccc' }}>
+            {msg}
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+};
+
+export default Chat;
